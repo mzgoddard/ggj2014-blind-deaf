6753cc92698b5b45548fc61d51ad3e75e11c64b7
bmac <bmac325@GMail.com>
2014-01-25 14:00:08 -0500

    Create server side messages for updating, position, level and actions

--- server/index.js
+++ server/index.js
@@ -6,6 +6,19 @@ var connect = require('connect');
 var expressServer = require('./express_server');
 var roomUtils = require('./room_utils');
 
+var messageOtherMembers = function(user, message, data){
+  var room = user.getRoom();
+  var members = room.getMembers();
+
+  var otherMembers = members.filter(function(member) {
+    return member !== user;
+  });
+
+  otherMembers.forEach(function(member) {
+    member.message(message, data);
+  });
+};
+
 cloak.configure({
   express: expressServer,
   autoJoinLobby: false,
@@ -17,7 +30,20 @@ cloak.configure({
 
     room: {
       close: roomUtils.cleanUpRoomOnClose
-    }
+    },
+
+    reportPosition: function(user, data) {
+      messageOtherMembers(user, 'reportPosition', data);
+    },
+
+    reportLevelChange: function(data, user) {
+      console.log(user);
+      user.getRoom().messageMembers('reportLevelChange', data);
+    },
+
+    reportAction: function(user, data) {
+      user.getRoom().messageMembers('reportAction', data);
+    },
   }
 });
 
--- src/networking.js
+++ src/networking.js
@@ -5,6 +5,11 @@ require('cloak-client');
 
 cloak.configure({
   messages: {
+    reportPosition: console.log.bind(console),
+
+    reportLevelChange: console.log.bind(console),
+
+    reportAction: console.log.bind(console),
 
   },
 
@@ -13,8 +18,21 @@ cloak.configure({
       cloak.message('joinRoom', window.location.pathname);
     },
 
-    joinedRoom: console.log.bind(console)
+    joinedRoom: console.log.bind(console),
   }
 });
 
 cloak.run('localhost');
+
+module.exports.reportPosition = function(player) {
+  var data = _.pick(player.entity._ops, 'x', 'y', 'rotation');
+  cloak.message('reportPosition', JSON.stringify(data));
+};
+
+module.exports.reportLevelChange = function(newLevel) {
+  cloak.message('reportLevelChange', newLevel);
+};
+
+module.exports.reportAction = function(action) {
+  cloak.message('reportAction', action);
+};
