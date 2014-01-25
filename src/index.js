645e7897c78f76dd01d8a0903d057ced61995ea7
Michael "Z" Goddard <mzgoddard@gmail.com>
2014-01-25 10:47:19 -0500

    Add PlayerActor, player actor and level0 data, and create that level.

---
+++ src/index.js
@@ -0,0 +1,17 @@
+// Load actor types so that they register.
+require('./actor.player');
+
+// Load the level constructor.
+var Level = require('./level');
+
+// Create the first level.
+new Level('level0');
+
+// Dynamically resize canvas
+function resize() {
+  var canvas = document.querySelector('canvas');
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
+window.addEventListener('resize', resize);
+resize();
