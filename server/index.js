9416e37af9e7572981b45c6473dd374c96d39ef4
bmac <bmac325@GMail.com>
2014-01-25 11:34:12 -0500

    Clean up some unused variables and disable the lobby

--- server/index.js
+++ server/index.js
@@ -1,30 +1,15 @@
 /* jshint node:true */
 
 var cloak = require('cloak');
-var _ = require('underscore');
 var connect = require('connect');
 
-var express = require('express');
-var app = express();
-app.use(express.static('dist'));
-app.get('*', function (req, res) {
-  res.sendfile('./dist/index.html');
-});
-var expressServer = app.listen(3000);
-
-
+var expressServer = require('./express_server');
 var roomUtils = require('./room_utils');
-
-var clientPort = 8080;
-var serverPort = 8090;
 
 cloak.configure({
   express: expressServer,
+  autoJoinLobby: false,
   messages: {
-    chat: function(msg, user) {
-      user.getRoom().messageMembers('chat', msg);
-    },
-
     joinRoom: function(roomName, user) {
       var room = roomUtils.getRoomForUrl(cloak, roomName);
       room.addMember(user);
